1190bf1d98c52715fab39c2fcbf6a86c3ca68777
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-24 09:02:24 +0200

    Created a game class with simple logic

---
+++ ConsoleUI/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class Game
+    {
+        private string GameTitle = "Trivia Quest";
+        private string GameTitleArt = @"Trivia Quest";
+        private string GameDescription = @"Welcome to Trivia Quest
+Battle your friends for the top score in silly trivia.";
+
+        public void Start()
+        {
+            Title = GameTitle; 
+            Write("What is your name: ");
+            string name = ReadLine();
+
+            WriteLine($"Welcome to trivia {name}");
+
+            ReadKey(true);
+
+        }
+        
+    }
+}
