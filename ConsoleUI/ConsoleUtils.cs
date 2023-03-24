8d9a21a3c0b80443a7fd8b5e7afa9a28c3826999
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-24 10:44:17 +0200

    Created a utils class

---
+++ ConsoleUI/ConsoleUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class ConsoleUtils
+    {
+        public static void WaitForKey()
+        {
+     
+            WriteLine("\n(Press any key to continue...)");
+            ReadKey(true);
+        }
+
+        public static void QuitApplication()
+        {
+
+            WriteLine("\nPress any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
